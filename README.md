942cc08e007a33a3a3aa68a832dc09e957f4ab19
DanielRH <dholguin2042@gmail.com>
2025-11-09 22:35:14 -0500

    Sprint 8 – Etapa 1: initialCards y README actualizado

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Tripleten web_project_around_es
+
+Proyecto del bootcamp de Desarrollo Web de TripleTen.  
+Es una página donde se muestran tarjetas con fotos de lugares en los EE.UU.
+
+En esta etapa (Sprint 8 – Etapa 1) se hizo:
+
+- Creación de la carpeta `scripts` y del archivo `scripts/index.js`.
+- Conexión del script en `index.html`.
+- Definición del array `initialCards` con seis tarjetas (name + link).
+- Uso de `initialCards.forEach()` para mostrar los nombres de las tarjetas en la consola.
+
+## Tecnologías
+
+- HTML
+- CSS
+- JavaScript
+
+## Autor
+
+**Daniel Ramirez Holguin**  
+GitHub: [DRH2042](https://github.com/DRH2042)
---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+// scripts/index.js
+// scripts/index.js
+
+// 1. Array de tarjetas iniciales
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
+// 2. Mensajes para verificar que todo está bien
+console.log("scripts/index.js conectado correctamente ✅");
+console.log("initialCards:", initialCards);
+console.log("Número de tarjetas iniciales:", initialCards.length);
+
+// 3. Recorrer el array e imprimir solo los nombres en consola
+initialCards.forEach((card) => {
+  console.log(card.name);
+});
